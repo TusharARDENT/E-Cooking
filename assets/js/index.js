e88303e2e1f7615185c353f05d79a755fadb1c60
TusharArdent <tushar.kadam@ardentisys.com>
2024-12-10 22:40:34 +0530

    Added functionality to create list items through index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,45 @@
-// Functionality of the page should appear here
+const api = "https://dummyjson.com/recipes?limit=12";
+let globalData;
+
+async function getdata() {
+  // Fetch data
+  await fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      globalData = data.recipes;
+      console.log(globalData);
+      updateDOM();
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+function updateDOM() {
+  const cardContainer = document.querySelector(".cardList");
+  cardContainer.innerHTML = "";
+  //forEach Loop
+  globalData.forEach((recipe) => {
+    const cardItem = document.createElement("li");
+    cardItem.className = "cardListItem";
+
+    cardItem.innerHTML = `
+      <figure>
+        <img src="${recipe.image}" alt="${recipe.name}">
+      </figure>
+      <div class="cardDesc">
+        <div class="cardHeading">
+          <h4 class="cardTitle">${recipe.name}</h4>
+          <span class="cookName">${recipe.cuisine}</span>
+          <span class="cardTag">${recipe.difficulty}</span>
+        </div>
+        <div class="timeEstimation">
+          <span class="timeToPrepare">${recipe.prepTimeMinutes} min | ${recipe.rating} / 5.0 | ${recipe.reviewCount} reviews</span>
+          <span class="saveIcon"><span>Save Icon</span></span>
+        </div>
+      </div>
+    `;
+
+    cardContainer.appendChild(cardItem);
+  });
+}
+
+getdata();
